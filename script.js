aae176898a9ee05b42936303ac20b0ba3823db81
divyesh <bamaniyadivyesh2002@gmail.com>
2024-09-11 19:59:59 +0530

    Logic for the case when No country is found when Searched

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
 const CountryContainer = document.querySelector(".countries-container");
 const filterByRegion = document.querySelector(".filter-by-region");
+const searchInput = document.querySelector(".Search-input");
+const img = document.querySelector(".errorImage");
 
+// Initial Data for all countries
 fetch("https://restcountries.com/v3.1/all")
     .then(response => response.json())
     .then(renderCountries)
 
+// Filter By Region
 filterByRegion.addEventListener('change', e=>{
   
-fetch(`https://restcountries.com/v3.1/region/${e.target.value}`)
+    CountryContainer.innerHTML = "";
+    fetch(`https://restcountries.com/v3.1/region/${e.target.value}`)
     .then(response => response.json())
     .then(renderCountries)
+})
+
+// Search By Country Name
+searchInput.addEventListener('change',(e)=>{
+
     CountryContainer.innerHTML = "";
+    fetch(`https://restcountries.com/v3.1/name/${e.target.value}`)
+    .then(response => response.json())
+    .then(renderCountries)
+    .catch(
+        // Function shows when No country is found as inserted
+        ()=>{
+            const imgNotFound =  document.createElement("img");
+            imgNotFound.src = "./images/not-found.png";
+            imgNotFound.style.width = "250px";
+            imgNotFound.style.height = "300px";
+            img.style.display = "block";
+            CountryContainer.innerHTML = "";
+            img.appendChild(imgNotFound);
+        }
+    )
+    
 })
 
 function renderCountries(data){
+
+    if(data == ""){
+        console.log("Not found");
+        
+    }
     data.forEach(countries => {
         
         const countryCard = document.createElement('a');
