97f5f2e2ee2495c637417fe0bc90ab0c9dd467a0
divyesh <bamaniyadivyesh2002@gmail.com>
2024-09-12 18:07:17 +0530

    Removed logic of Showing Error when No country is found

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const CountryContainer = document.querySelector(".countries-container");
 const filterByRegion = document.querySelector(".filter-by-region");
 const searchInput = document.querySelector(".Search-input");
-const img = document.querySelector(".errorImage");
+const imgDiv = document.querySelector(".errorImage");
 const darkButton = document.querySelector(".DarkMode");
 const darkIcon = document.querySelector(".Dark-icon");
 const modeText = document.querySelector("[data-modeText]");
@@ -9,75 +9,73 @@ const modeText = document.querySelector("[data-modeText]");
 let allCountriesData
 
 // Dark Button Logic
-darkButton.addEventListener('click',()=>{    
+darkButton.addEventListener('click', () => {
     document.body.classList.toggle("dark");
-    
-    if(document.body.classList.contains("dark")){
-    darkIcon.setAttribute("class", "fa-regular fa-sun");
-    modeText.innerText = "Light Mode";
+
+    if (document.body.classList.contains("dark")) {
+        darkIcon.setAttribute("class", "fa-regular fa-sun");
+        modeText.innerText = "Light Mode";
     }
 
-    else{
-      darkIcon.setAttribute("class", "Dark-icon fa-regular fa-moon fa-rotate-by");
-      modeText.innerText = "Dark Mode";  
-    } 
+    else {
+        darkIcon.setAttribute("class", "Dark-icon fa-regular fa-moon fa-rotate-by");
+        modeText.innerText = "Dark Mode";
+    }
 })
 
 // Initial Data for all countries
 fetch("https://restcountries.com/v3.1/all")
     .then(response => response.json())
-    .then((data)=>{
+    .then((data) => {
         renderCountries(data);
         allCountriesData = data;
     })
 
 // Filter By Region
-filterByRegion.addEventListener('change', e=>{
-  
+filterByRegion.addEventListener('change', e => {
+
     CountryContainer.innerHTML = "";
     fetch(`https://restcountries.com/v3.1/region/${e.target.value}`)
-    .then(response => response.json())
-    .then(renderCountries)
+        .then(response => response.json())
+        .then(renderCountries)
 })
 
 // Search By Country Name
-searchInput.addEventListener('input',(e)=>{
-
+searchInput.addEventListener('input', (e) => {
 
     CountryContainer.innerHTML = "";
 
-    const filteredCountries = allCountriesData.filter(country=> {
+    const filteredCountries = allCountriesData.filter(country => {
         return country.name.common.toLowerCase().includes(e.target.value.toLowerCase())
     }
-)
+    )
+
     renderCountries(filteredCountries)
-    
+
 })
 
-
-
 // Dark-Mode implementation
-darkButton.addEventListener('click', ()=>{
+darkButton.addEventListener('click', () => {
 
 })
 // Function for rendering Data
-function renderCountries(data){
+function renderCountries(data) {
 
     data.forEach(countries => {
-        
+
         const countryCard = document.createElement('a');
         countryCard.classList.add("country-card");
         countryCard.href = `./country.html?name=${countries.name.common}&fullText=true`;
-      
+
         countryCard.innerHTML =
-                    `<img src="${countries.flags.svg}" alt="Flag">
+            `<img src="${countries.flags.svg}" alt="Flag">
                     <div class="card-text">
                         <h3 class="card-title">${countries?.name?.common}</h3>
                         <p><strong>Population: </strong>${countries?.population.toLocaleString("en-In")}</p>
                         <p><strong>Region: </strong>${countries?.region}</p>
-                        <p><strong>Capital: </strong>${countries?.capital?countries?.capital.join(", "):"No capital"}</p>
+                        <p><strong>Capital: </strong>${countries?.capital ? countries?.capital.join(", ") : "No capital"}</p>
                     </div>`
-        CountryContainer.appendChild(countryCard);          
-        }
+        CountryContainer.appendChild(countryCard);
+    }
     )
 }
